eb798acbea81e123969aad9e008a977e61e5f605
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-27 01:01:13 +0200

    Task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,8 +1,8 @@
-//#!/usr/bin/node
+#!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
@@ -16,8 +16,8 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
