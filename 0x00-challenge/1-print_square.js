1081c6a27c38898cde2c6ad3a34831421009be1e
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-27 01:25:14 +0200

    Task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+//#!/usr/bin/node
 /*
     Print a square with the character #
 
@@ -14,10 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+let size = parseInt(process.argv[2], 10)
 
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
